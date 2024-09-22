24bd07929ee814e4066775a5495a7551c06836dc
phuvinhnguyen <phuvinhnguyen812002@gmail.com>
2024-09-22 08:29:48 +0200

    update code

--- alarm.py
+++ alarm.py
@@ -6,7 +6,7 @@ def trigger_light():
     # To be implemented: Trigger light as an alert
     pass
 
-def alert_protocol():
+def alert_protocol() -> None:
     # Function to handle alert mechanism (play sound, trigger light, etc.)
     print("Alert triggered!")
     play_sound()
--- gyroscope.py
+++ gyroscope.py
@@ -2,6 +2,11 @@ def read_gyroscope():
     # To be implemented: Read data from the gyroscope/accelerometer
     pass
 
-def check_fall_condition(gyro_data):
-    # Placeholder function to process gyroscope data and determine fall
+def check_fall_condition(gyro_data) -> bool:
+    '''
+    Placeholder function to process gyroscope data and determine fall
+    Return:
+        bool: True if fall detected, False otherwise
+    '''
+
     pass
