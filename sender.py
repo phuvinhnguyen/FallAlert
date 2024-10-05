c6eb828b3f71d5b40d33fd266f3cd4bdfc2cdb59
phuvinhnguyen <81418873+phuvinhnguyen@users.noreply.github.com>
2024-10-05 18:02:13 +0200

    Add files via upload

---
+++ sender.py
@@ -0,0 +1,21 @@
+from OnlineDatabase import GitHubManager
+import requests
+import json
+
+GITHUB_TOKEN = 'YOUR_GITHUB_TOKEN'
+
+def get_location():
+    try:
+        response = requests.get('http://ipinfo.io/')
+        data = response.json()
+        return json.dumps(data, indent=4)
+    except requests.RequestException as e:
+        return json.dumps({'error': 'Failed to get information from IP geolocation service'}, indent=4)
+
+def public_calling():
+    manager = GitHubManager(GITHUB_TOKEN)
+    manager.push(
+        'phuvinhnguyen/FallAlert',
+        f'customer/warning.txt',
+        get_location()
+        )
